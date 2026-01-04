1f6bfab6c51577e9be48b3bc837031f94d15d374
Akinola Kehinde <khennyakin@gmail.com>
2026-01-04 16:26:33 +0100

    feat: add verification badge to user's picture on profile settings page

--- src/pages/ProfileSettings/ProfileSettings.tsx
+++ src/pages/ProfileSettings/ProfileSettings.tsx
@@ -7,12 +7,13 @@ import mobile from "../../assets/images/mobile-icon.svg"
 import email from "../../assets/images/email-icon.svg"
 import key from "../../assets/images/key-icon.svg"
 import right from "../../assets/images/right.svg"
+import shield from "../../assets/images/shield-check-fill.svg"
 
 function ProfileSettings() {
   return (
     <main className="text-[#191919] leading-[150%] tracking-[0%] pt-7">
         <div className="flex items-center px-4 mb-10">
-            <Link to="" className='text-[#1D62CA] font-semibold text-sm flex items-center gap-1'>
+            <Link to="/" className='text-[#1D62CA] font-semibold text-sm flex items-center gap-1'>
                 <img src={left} alt="icon" />
                 Back
             </Link>
@@ -21,8 +22,9 @@ function ProfileSettings() {
         </div>
 
         <div className="flex flex-col items-center gap-1 mb-10">
-            <div className="border-2 border-[#4FBCA8] w-26 h-26 rounded-full relative overflow-hidden flex items-center justify-center">
+            <div className="border-2 border-[#4FBCA8] w-26 h-26 rounded-full relative flex items-center justify-center">
                 <img src={pp} alt="profile-p" className='w-24 h-24 rounded-full' />
+                <img src={shield} alt="shield-icon" className='absolute right-1 bottom-0' />
             </div>
             <h4 className="text-sm font-semibold">Abdullah Ghatasheh</h4>
             <p className="text-[#78838D] text-xs">Joined 2 years ago</p>
